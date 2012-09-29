ae99d5cd342414ab073a39d8251f645a68f88a39
toctoc352 <egm352@gmail.com>
2012-09-29 21:04:36 +0900

    リファクタリング(CardTest)

--- CardPlaying/src/CardTest.java
+++ CardPlaying/src/CardTest.java
@@ -9,9 +9,8 @@ public class CardTest {
 	//スペードのエースのプロパティチェック
 	public void testCheckAceOfSpadePropatys() {
         int value = 1;
-		Card card = Card.ACE_OF_SPADE;
-		assertEquals(Card.Suit.SPADE, card.getSuit());
-		assertEquals(value, card.getValue());
+		assertEquals(Card.Suit.SPADE, Card.ACE_OF_SPADE.getSuit());
+		assertEquals(value, Card.ACE_OF_SPADE.getValue());
 	}
 
 }
