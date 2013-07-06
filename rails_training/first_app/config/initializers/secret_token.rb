8a624bc49c5a194d6f06406e7e8c79ddc64bf28a
toctoc352 <egm352@gmail.com>
2013-07-06 13:46:47 +0900

    InInitial commit Rails Training App

---
+++ rails_training/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '545bcc8018c704bafbefff4c5c95ea8f90277a616b59108f9d005c1a3b82349e8264ccaea446d6a19e4f166da70662bea37f507d684f1cbb909845585d1f62bd'
