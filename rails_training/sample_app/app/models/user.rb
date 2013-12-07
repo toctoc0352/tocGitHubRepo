bb0f249c1b768e12cefd311e9c024d4aa5d28b07
toctoc352 <egm352@gmail.com>
2013-12-07 14:13:16 +0900

    Railsチュートリアル  第6章  〜6.2.4 フォーマットを検証する（※途中）

--- rails_training/sample_app/app/models/user.rb
+++ rails_training/sample_app/app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  # attr_accessible :title, :body
+  attr_accessible :email, :name
+  
+  validates :name, presence:true, length:{ maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:true, fomat: {with:VALID_EMAIL_REGEX}
 end
--- rails_training/sample_app/spec/models/user_spec.rb
+++ rails_training/sample_app/spec/models/user_spec.rb
@@ -1,5 +1,55 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: "Exsample User", email: "user@exsample.com") }
+  
+  subject { @user }
+  
+  it { should respond_to(:name) }
+  #下記のように書くこともできる
+  #it 'User should respond to name' do 
+  #   @user.should respond_to(:name)
+  #end
+    
+  it { should respond_to(:email) }
+  
+  #@userオブジェクトが有効かどうか
+  #@user.valid? の実行と同義
+  it { should be_valid }
+  
+  describe "when name is not presence" do
+    before {@user.name = " "}
+    it { should_not be_valid }
+  end
+
+  describe "when email is not presence" do
+    before {@user.email = " "}
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before {@user.name = "a"*51}
+    it { should_not be_valid }
+  end
+    
+  describe "when email format invalid" do
+    it "should be invalid" do
+      addresses = %w[ user@foo,com user_at_foo.org exsampe.user@foo.foo@bar_baz.com foo@bar+baz.com ]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
+  
+  describe "when email format valid" do
+    it "should be valid" do
+      addresses = %w[ user@foo.COM A_US_ER@f.b.org first.lst@foo.jo a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
+  end
+  
 end
