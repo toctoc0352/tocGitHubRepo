f790f8813d7811494409ad69152c99d455f7ff4a
toctoc352 <egm352@gmail.com>
2013-12-09 18:28:10 +0900

    Railsチュートリアル 第6章 6.3セキュアなパスワードを追加する　〜6.3.4　ユーザがセキュアなパスワードを持っている

--- rails_training/sample_app/db/schema.rb
+++ rails_training/sample_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131209083630) do
+ActiveRecord::Schema.define(:version => 20131209090000) do
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(:version => 20131209083630) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
