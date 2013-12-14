ed9e3dfe46a51b439e16767a3f1c4e6a951e6af4
toctoc352 <egm352@gmail.com>
2013-12-14 21:32:33 +0900

    Railsチュートリアル 第7章  7.2.1ユーザー登録のためのテスト

--- rails_training/sample_app/spec/requests/user_pages_spec.rb
+++ rails_training/sample_app/spec/requests/user_pages_spec.rb
@@ -5,10 +5,38 @@ describe "UserPages" do
   subject { page }
 
   describe "signup page" do
+     before { visit signup_path }
+ 
+     it { should have_selector('h1',    text: 'Sign up') }
+     it { should have_selector('title', text: 'Sign up') }
+  end
+  
+  describe "signup" do
     before {visit signup_path}
-
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title',    text: full_title('Sign up')) }
+    
+    let(:submit) { "Create my account" }
+    
+    #無効なユーザの登録
+    describe "with invalid infomation" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end    
+    end
+    
+    #有効なユーザの登録
+    describe "with valid infomation" do
+      before do
+        #各入力項目を入力
+        fill_in "Name",               with: "Example User"
+        fill_in "Email",              with: "user@exsample.com"
+        fill_in "Password",           with: "foobar"
+        fill_in "Confirmation",       with: "foobar"
+      end
+      
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end    
+    end  
   end
   
   describe "profile page" do
