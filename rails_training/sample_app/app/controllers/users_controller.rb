a3fee04e0c452c6b4da367c71b489e480fad7bde
toctoc352 <egm352@gmail.com>
2013-12-14 22:35:23 +0900

    Railsチュートリアル 第7章  7.4ユーザ登録成功

--- rails_training/sample_app/app/controllers/users_controller.rb
+++ rails_training/sample_app/app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # Handle a successful save.
+      # ユーザ表示ページへ移動
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
