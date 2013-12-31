a1468e1a117c790e50c536917488f7c99676aee9
toctoc352 <egm352@gmail.com>
2013-12-31 18:01:02 +0900

    Railsチュートリアル 第8章 〜8.2.2 現在のユーザ

--- rails_training/sample_app/app/helpers/sessions_helper.rb
+++ rails_training/sample_app/app/helpers/sessions_helper.rb
@@ -7,4 +7,18 @@ module SessionsHelper
       cookies.permanent[:remember_token] = user.remember_token
       self.current_user = user
   end 
+  
+  #current_userへの要素代入を定義
+  def current_user=(user)
+    @current_user = user    
+  end
+
+  #current_userの取得
+  def current_user
+    #||= (or equal)
+    # A ||= B
+    #   AがnilならBを代入、そうでなければ代入しない
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])    
+  end
+  
 end
