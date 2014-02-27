7f1c49320f6fbc200f3bad721259aaf610920c5a
江上剛志 <egm352@gmail.com>
2014-02-28 00:51:56 +0900

    Railsチュートリアル 第11章 〜 11.2フォローしているユーザー用のWebインターフェイス 11.2.4 [フォローする] ボタン (標準的な方法)

--- rails_training/sample_app/spec/requests/authentication_pages_spec.rb
+++ rails_training/sample_app/spec/requests/authentication_pages_spec.rb
@@ -113,7 +113,20 @@ describe "Authentication" do
           before {delete micropost_path(FactoryGirl.create(:micropost))}
           specify { response.should redirect_to(signin_path) }  
         end
-      end      
+      end
+      
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }  
+        end
+
+        describe "submitting to the delete action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }  
+        end
+      end
+            
     end
     
     describe "as wrong user" do
